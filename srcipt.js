c9dd138ba9e045162a82e91d0205e90c70dcbe68
olajesu23 <olaoluwaf2000@gmail.com>
2023-03-29 12:20:36 +0100

    launchcountdown

---
+++ srcipt.js
@@ -0,0 +1,29 @@
+// let countDown = new Date("mar 25, 2023  :00:00").getTime();
+// let myCountDown = setInterval(function(){
+
+// }, 1000)
+// let now = new Date().getTime();
+// let timeLeft = countDown - now;
+// let days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+// let hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) /(1000 * 60 * 60));
+// let minutes = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) /(1000 * 60))
+// let seconds = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) /(1000 ))
+
+// console.log(Date())
+
+
+
+// document.querySelector(".days").innerHTML = days
+// document.querySelector(".hours").innerHTML =hours
+// document.querySelector(".minutes").innerHTML =minutes
+// document.querySelector(".seconds").innerHTML =seconds
+
+// if(timeLeft < 0){
+//     clearInterval(countDown);
+//     document.querySelector(".days").innerHTML =""
+//     document.querySelector(".hours").innerHTML =""
+//     document.querySelector(".minutes").innerHTML =""
+//     document.querySelector(".seconds").innerHTML ="HAPPY LAUNCHING"
+
+// }
+
